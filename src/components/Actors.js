6249e8524386fa0520a7bfb5e62d7f7f3c869c1a
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-08-09 10:49:44 +0000

    Fixed the react basic route lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const listActors = actors.map((actor) =>{
+    return(
+      <div key={actor.name}>
+        {actor.name}
+        <ul>
+          {actor.movies.map(movie => <li key={movie}>{movie}</li>)}
+        </ul>
+      </div>
+    )
+  })
+  return (
+  <div>{/*{code here}*/}
+  <h1>Actors Page</h1>
+  {listActors}
+  </div>
+  )
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,59 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const myNavStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+  return (
+  <div className="navbar">{/*{code here}*/}
+      <NavLink 
+        to="/" 
+        exact 
+        style={myNavStyles}
+        activeStyle={
+          {background: "brown"}
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink 
+        to="/Movies" 
+        exact 
+        style={myNavStyles}
+        activeStyle={
+          {background: "brown"}
+        }
+      >
+        Movies
+      </NavLink>
+      <NavLink 
+        to="/Directors" 
+        exact 
+        style={myNavStyles}
+        activeStyle={
+          {background: "brown"}
+        }
+      >
+        Directors
+      </NavLink>
+      <NavLink 
+        to="/Actors" 
+        exact 
+        style={myNavStyles}
+        activeStyle={
+          {background: "brown"}
+        }
+      >
+        Actors
+      </NavLink>
+  </div>
+  )
 }
 
 export default NavBar;
